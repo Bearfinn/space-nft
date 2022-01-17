1385b399934b1ca30e4f2239a29f78ed7c618118
Kritsada Sunthornwutthikrai <kritsada.stw@gmail.com>
2022-01-17 19:49:05 +0700

    feat: Add temporary shop page

---
+++ components/shop/ShopCard.tsx
@@ -0,0 +1,31 @@
+import Button from "components/base/Button";
+import { FunctionComponent } from "react";
+
+interface ShopCardProps {
+  title: string;
+  description: string;
+  action: string;
+}
+
+const ShopCard: FunctionComponent<ShopCardProps> = ({
+  title,
+  description,
+  action,
+}) => {
+  return (
+    <div className="rounded-lg text-center bg-gray-900 bg-opacity-75 h-[300px] p-8 hover:shadow-lg hover:shadow-teal-300/50 transition opacity-80 hover:opacity-100">
+      <div className="text-center flex flex-col items-center h-full">
+        <div className="flex-grow">
+          <div className="font-mono">{title}</div>
+          <div className="mt-4 font-light">{description}</div>
+        </div>
+
+        <div className="mt-4">
+          <Button>{action}</Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ShopCard;
--- pages/shop/index.tsx
+++ pages/shop/index.tsx
@@ -1,9 +1,43 @@
+import ShopCard from "components/shop/ShopCard";
 import { FunctionComponent } from "react";
 
 interface ShopPageProps {}
 
 const ShopPage: FunctionComponent<ShopPageProps> = () => {
-  return <div></div>;
+  return (
+    <div className="container mx-auto max-w-4xl">
+      <div className="grid grid-cols-2 gap-8">
+        <div className="col-span-1">
+          <ShopCard
+            title="GRB Token"
+            description="GRB token is used for several activities"
+            action="Buy with AVAX"
+          ></ShopCard>
+        </div>
+        <div className="col-span-1">
+          <ShopCard
+            title="Fuel"
+            description="Fuel drives up your ships"
+            action="Buy with GRB"
+          ></ShopCard>
+        </div>
+        <div className="col-span-1">
+          <ShopCard
+            title="Booster Pack"
+            description="Get randomized ships"
+            action="Buy with Crystal"
+          ></ShopCard>
+        </div>
+        <div className="col-span-1">
+          <ShopCard
+            title="Additional Inventory Slot"
+            description="More items!"
+            action="Buy with GRB"
+          ></ShopCard>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ShopPage;
