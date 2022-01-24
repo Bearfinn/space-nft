8584c103b9b2190ef7a1290999de756095b9db01
Kritsada Sunthornwutthikrai <kritsada.stw@gmail.com>
2022-01-24 22:22:27 +0700

    feat: Add filter to only show addresses for contract

--- hooks/useInventory.tsx
+++ hooks/useInventory.tsx
@@ -1,7 +1,5 @@
 import { useEffect, useState } from "react";
-import {
-  useMoralis, useNFTBalances
-} from "react-moralis";
+import { useMoralis, useNFTBalances } from "react-moralis";
 import { IShipMetadata, Ship } from "types/Items";
 import { useContract } from "./useContract";
 
@@ -28,76 +26,78 @@ export const useNFTs = () => {
       const upgradeCards: any[] = [];
       const ships: any[] = [];
 
-      const promises = data?.result.map(async (nft) => {
-        let metadata: any = nft.metadata;
-        if (!metadata && nft.token_uri) {
-          metadata = await window
-            .fetch(nft.token_uri)
-            .then((response) => response.json());
-        }
-        const tokenId = Number(nft.token_id);
+      const promises = data?.result
+        .filter((nft) => nft.token_address === contractAddress)
+        .map(async (nft) => {
+          let metadata: any = nft.metadata;
+          if (!metadata && nft.token_uri) {
+            metadata = await window
+              .fetch(nft.token_uri)
+              .then((response) => response.json());
+          }
+          const tokenId = Number(nft.token_id);
 
-        // Tokens
-        if (tokenId === 0) {
-          setMineral(Number(Moralis.Units.FromWei(nft.amount || "0")));
-        }
-        if (tokenId === 1) {
-          setCrystal(Number(Moralis.Units.FromWei(nft.amount || "0")));
-        }
-        if (tokenId === 2) {
-          setFuel(Number(nft.amount || "0"));
-        }
+          // Tokens
+          if (tokenId === 0) {
+            setMineral(Number(Moralis.Units.FromWei(nft.amount || "0")));
+          }
+          if (tokenId === 1) {
+            setCrystal(Number(Moralis.Units.FromWei(nft.amount || "0")));
+          }
+          if (tokenId === 2) {
+            setFuel(Number(nft.amount || "0"));
+          }
 
-        // Avatars
-        if (tokenId >= 4 && tokenId <= 11) {
-          avatars.push({
-            amount: parseInt(nft.amount || "0"),
-            name: metadata.result.name,
-          });
-        }
+          // Avatars
+          if (tokenId >= 4 && tokenId <= 11) {
+            avatars.push({
+              amount: parseInt(nft.amount || "0"),
+              name: metadata.result.name,
+            });
+          }
 
-        // Lands
-        if (tokenId >= 12 && tokenId <= 21) {
-          lands.push({
-            amount: parseInt(nft.amount || "0"),
-            name: metadata.result.name,
-          });
-        }
+          // Lands
+          if (tokenId >= 12 && tokenId <= 21) {
+            lands.push({
+              amount: parseInt(nft.amount || "0"),
+              name: metadata.result.name,
+            });
+          }
 
-        // Upgrade cards
-        if (tokenId >= 22 && tokenId <= 33) {
-          upgradeCards.push({
-            amount: parseInt(nft.amount || "0"),
-            name: metadata.result.name,
-          });
-        }
+          // Upgrade cards
+          if (tokenId >= 22 && tokenId <= 33) {
+            upgradeCards.push({
+              amount: parseInt(nft.amount || "0"),
+              name: metadata.result.name,
+            });
+          }
 
-        // Ships
-        if (tokenId >= 34) {
-          const ship: IShipMetadata = metadata.result;
-          const getAttributes = (traitType: string) => {
-            const attribute = ship.attributes.find(
-              (attribute) => attribute.trait_type === traitType
-            );
-            return attribute?.value || null;
-          };
-          ships.push({
-            tokenId,
-            amount: parseInt(nft.amount || "0"),
-            name: ship.name,
-            description: ship.description,
-            src: ship.image,
-            type: "Ship",
-            hp: getAttributes("Hit Point"),
-            attack: getAttributes("Attack"),
-            travelSpeed: getAttributes("Travel Speed"),
-            miningSpeed: getAttributes("Mining Speed"),
-            shipType: getAttributes("Ship Type"),
-            skinId: getAttributes("Skin Id"),
-            color: getAttributes("Color")
-          } as Ship);
-        }
-      });
+          // Ships
+          if (tokenId >= 34) {
+            const ship: IShipMetadata = metadata.result;
+            const getAttributes = (traitType: string) => {
+              const attribute = ship.attributes.find(
+                (attribute) => attribute.trait_type === traitType
+              );
+              return attribute?.value || null;
+            };
+            ships.push({
+              tokenId,
+              amount: parseInt(nft.amount || "0"),
+              name: ship.name,
+              description: ship.description,
+              src: ship.image,
+              type: "Ship",
+              hp: getAttributes("Hit Point"),
+              attack: getAttributes("Attack"),
+              travelSpeed: getAttributes("Travel Speed"),
+              miningSpeed: getAttributes("Mining Speed"),
+              shipType: getAttributes("Ship Type"),
+              skinId: getAttributes("Skin Id"),
+              color: getAttributes("Color"),
+            } as Ship);
+          }
+        });
 
       await Promise.all(promises);
       setNfts(ships);
